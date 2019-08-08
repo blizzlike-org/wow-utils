32237bfc6bbadf7dfd2714e76b62a7d4262577e8
crito <crito@blizzlike.org>
2019-08-08 22:44:50 +0200

    dbcspec: add spec file for AnimationData.dbc

---
+++ dbcspec/animationdata.lua
@@ -0,0 +1,16 @@
+return {
+  [5875] = {
+    rcount = 208,
+    fcount = 7,
+    rsize = 28,
+    fields = {
+      { name = "id", type = "uint32_t", count = 1 },
+      { name = "name", type = "string", count = 1 },
+      { name = "weaponFlags", type = "int32_t", count = 1 },
+      { name = "bodyFlags", type = "int32_t", count = 1 },
+      { name = "flags", type = "int32_t", count = 1 },
+      { name = "fallback", type = "uint32_t", count = 1 },
+      { name = "behavior", type = "uint32_t", count = 1 }
+    }
+  }
+}
--- dbcspec/faction.lua
+++ dbcspec/faction.lua
@@ -4,13 +4,13 @@ return {
     fcount = 37,
     rsize = 148,
     fields = {
-      { name = "ID", type = "uint32_t", count = 1 },
-      { name = "reputationIndex", type = "uint32_t", count = 1 },
-      { name = "reputationRaceMask", type = "uint32_t", count = 4 },
-      { name = "reputationClassMask", type = "uint32_t", count = 4 },
-      { name = "reputationBase", type = "uint32_t", count = 4 },
-      { name = "reputationFlags", type = "uint32_t", count = 4 },
-      { name = "parentFactionID", type = "uint32_t", count = 1 },
+      { name = "id", type = "uint32_t", count = 1 },
+      { name = "reputationIndex", type = "int32_t", count = 1 },
+      { name = "reputationRaceMask", type = "int32_t", count = 4 },
+      { name = "reputationClassMask", type = "int32_t", count = 4 },
+      { name = "reputationBase", type = "int32_t", count = 4 },
+      { name = "reputationFlags", type = "int32_t", count = 4 },
+      { name = "parentFactionId", type = "uint32_t", count = 1 },
       { name = "name", type = "string", count = 8 },
       { name = "description", type = "string", count = 8 }
     }
