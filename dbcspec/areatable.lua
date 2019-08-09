6182ba300a8f07edd9620339720e72c25e561480
crito <crito@blizzlike.org>
2019-08-09 18:39:16 +0200

    dbcspec: add spec file for AreaTable and AreaTrigger

---
+++ dbcspec/areatable.lua
@@ -0,0 +1,25 @@
+return {
+  [5875] = {
+    fcount = 25,
+    rsize = 100,
+    fields = {
+      { name = "id", type = "uint32_t", count = 1 },
+      { name = "map", type = "uint32_t", count = 1 },
+      { name = "parentAreaTable", type = "uint32_t", count = 1 },
+      { name = "areaBit", type = "hex", count = 1 },
+      { name = "flags", type = "hex", count = 1 },
+      { name = "soundPreferences", type = "uint32_t", count = 1 },
+      { name = "soundPreferencesUnderwater", type = "uint32_t", count = 1 },
+      { name = "soundAmbience", type = "uint32_t", count = 1 },
+      { name = "zoneMusic", type = "uint32_t", count = 1 },
+      { name = "zoneIntroMusicTable", type = "uint32_t", count = 1 },
+      { name = "explorationLevel", type = "int32_t", count = 1 },
+      { name = "areaName", type = "string", count = 8 },
+      { name = "factionGroup", type = "uint32_t", count = 1 },
+      { name = "liquidType", type = "uint32_t", count = 1 },
+      { name = "minElevation", type = "int32_t", count = 1 },
+      { name = "ambientMultiplier", type = "float", count = 1 },
+      { name = "light", type = "uint32_t", count = 1 }
+    }
+  }
+}
---
+++ dbcspec/areatrigger.lua
@@ -0,0 +1,18 @@
+return {
+  [5875] = {
+    fcount = 10,
+    rsize = 40,
+    fields = {
+      { name = "id", type = "uint32_t", count = 1 },
+      { name = "map", type = "uint32_t", count = 1 },
+      { name = "locationX", type = "float", count = 1 },
+      { name = "locationY", type = "float", count = 1 },
+      { name = "locationZ", type = "float", count = 1 },
+      { name = "radius", type = "float", count = 1 },
+      { name = "boxLength", type = "float", count = 1 },
+      { name = "boxWidth", type = "float", count = 1 },
+      { name = "boxHeight", type = "float", count = 1 },
+      { name = "boxYaw", type = "float", count = 1 }
+    }
+  }
+}
