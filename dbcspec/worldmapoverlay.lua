8b259fd57145a5bcd787de23a28f862fab119c71
crito <crito@blizzlike.org>
2020-09-17 21:04:52 +0200

    dbcspec: add WorldMapOverlay specfile

---
+++ dbcspec/worldmapoverlay.lua
@@ -0,0 +1,22 @@
+return {
+  [5875] = {
+    fcount = 17,
+    rsize = 68,
+    fields = {
+      { name = "id", type = "uint32_t", count = 1 },
+      { name = "mapAreaID", type = "uint32_t", count = 1 },
+      { name = "areaID", type = "uint32_t", count = 4 },
+      { name = "x", type = "float", count = 1 },
+      { name = "y", type = "float", count = 1 },
+      { name = "name", type = "string", count = 1 },
+      { name = "textureWidth", type = "uint32_t", count = 1 },
+      { name = "textureHeight", type = "uint32_t", count = 1 },
+      { name = "offsetX", type = "uint32_t", count = 1 },
+      { name = "offsetY", type = "uint32_t", count = 1 },
+      { name = "hitRectTop", type = "uint32_t", count = 1 },
+      { name = "hitRectLeft", type = "uint32_t", count = 1 },
+      { name = "hitRectBottom", type = "uint32_t", count = 1 },
+      { name = "hitRectRight", type = "uint32_t", count = 1 }
+    }
+  }
+}
