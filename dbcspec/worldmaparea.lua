5c9b7b2fef7cd4d40f0456fbf3f4e55cdb55e4a4
crito <matthiashauber@googlemail.com>
2023-10-30 00:01:20 +0100

    dbcspec: add WorldMapArea specfile

---
+++ dbcspec/worldmaparea.lua
@@ -0,0 +1,16 @@
+return {
+  [5875] = {
+    fcount = 8,
+    rsize = 32,
+    fields = {
+      { name = "id", type = "uint32_t", count = 1 },
+      { name = "mapID", type = "uint32_t", count = 1 },
+      { name = "areaID", type = "uint32_t", count = 1 },
+      { name = "areaName", type = "string", count = 1 },
+      { name = "locLeft", type = "float", count = 1 },
+      { name = "locRight", type = "float", count = 1 },
+      { name = "locTop", type = "float", count = 1 },
+      { name = "locBottom", type = "float", count = 1 },
+    }
+  }
+}
